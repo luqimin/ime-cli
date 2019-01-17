7115e0c39b3a2744c848b762aac6ba26ac5170e6
Lu Qimin <qilu@163.com>
2019-01-17 14:53:31 +0800

    feat: 添加本地服务开启控制台日志

--- src/lib/watch.ts
+++ src/lib/watch.ts
@@ -3,6 +3,7 @@ import { BaseClass } from '../base';
 import Server from '../lib/server';
 import Builder from '../lib/build';
 import creatPack from '../util/packFactory';
+import log from '../util/log';
 
 class Watcher extends BaseClass<any> {
     protected async taskRunning() {
@@ -11,10 +12,13 @@ class Watcher extends BaseClass<any> {
 
         if (!fs.pathExistsSync(clientPath!) || !fs.pathExistsSync(serverPath!)) {
             // 打包所需文件
+            log.info('打包「dll」');
             await new Builder({ dllOnly: true }).run();
         }
 
+        log.info('项目运行前需要打包「client、ssr」，请稍后...');
         await Promise.all([pack.watchClient(), pack.watchSSR()]);
+        log.success('打包「client、ssr」结束');
 
         pack.watchServerStatic();
         pack.watchServer();
